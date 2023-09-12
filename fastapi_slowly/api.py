f367fd667263f7405de4d2bd39d1ca3f931e21c1
liquidcarbon <akscrps@gmail.com>
2023-09-12 11:28:35 -0600

    patch to sync responses with v0.7.1

--- fastapi_slowly/api.py
+++ fastapi_slowly/api.py
@@ -3,7 +3,7 @@
 from .version import get_version
 
 from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 import requests
 
 app = FastAPI()
@@ -13,15 +13,24 @@ app = FastAPI()
 async def git_time_travel(branch: str, endpoint: str|None = None):
     print(branch, endpoint)
     import os
-    os.system(f"git checkout {branch}")
+    os.system(f"git checkout {branch} && sleep 2")
     return RedirectResponse(f"/{endpoint or ''}")
 
 
-@app.get("/")
-async def root():
+@app.get("/hello")
+async def hello_iss():
     iss_url = "https://api.wheretheiss.at/v1/satellites/25544"
     response = requests.get(iss_url)
     return {
         "API version": get_version(),
         "Hello from ISS": response.json(),
     }
+
+
+@app.get("/")
+async def root():
+    html = f"""
+    <h1>FastAPI - Slowly: HTML Response</h1>
+    <p style="background-color:yellow;">version {get_version()}</p>
+    """
+    return HTMLResponse(content=html)
